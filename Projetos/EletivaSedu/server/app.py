b854fd4ffb60655a9b782493435889a0e84129ab
realpatocksx <159732016+realpatocksx@users.noreply.github.com>
2024-04-01 00:20:53 +0000

    commit

--- Projetos/EletivaSedu/server/app.py
+++ Projetos/EletivaSedu/server/app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request
 import os
 
-app = Flask(__name__, template_folder=os.path.abspath('Projetos/EletivaSedu/client/'), static_folder=os.path.abspath('Projetos/EletivaSedu/client/')) 
+app = Flask(__name__, template_folder=os.path.abspath('Projetos/EletivaSedu/client'), static_folder=os.path.abspath('Projetos/EletivaSedu/client')) 
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
