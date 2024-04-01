da23b197933eea1a1fc6942971576524f72bdb69
realpatocksx <159732016+realpatocksx@users.noreply.github.com>
2024-04-01 14:05:19 +0000

    commit

--- Projetos/EletivaSedu/server/app.py
+++ Projetos/EletivaSedu/server/app.py
@@ -3,10 +3,15 @@ import os
 
 app = Flask(__name__, template_folder=os.path.abspath('Projetos/EletivaSedu/client'), static_folder=os.path.abspath('Projetos/EletivaSedu/client')) 
 
+titulo = 'Teste da Sucao da segundo quinta'
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    titulo = 'Teste da Sucao da segundo quinta'
     return render_template('main/index.html', Titulo=titulo)
+
+@app.route('/secao/index.html')
+def secao():
+    return render_template('secao/index.html', Titulo1=titulo)
 
 if __name__ == '__main__':
     app.run(debug=True)
