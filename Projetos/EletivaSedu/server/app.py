a8c021a6fee9e725cd886fd01f9cfb0aa9b3cae1
realpatocksx <159732016+realpatocksx@users.noreply.github.com>
2024-04-05 13:29:30 +0000

    commit

--- Projetos/EletivaSedu/server/app.py
+++ Projetos/EletivaSedu/server/app.py
@@ -8,11 +8,15 @@ titulo2 = 'Vai Toma no Seu'
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('main/index.html', Titulo=titulo, Titulo2=titulo2)
+    return render_template('painel/index.html', Titulo=titulo, Titulo2=titulo2)
 
 @app.route('/secao/index.html')
 def secao():
     return render_template('secao/index.html', Titulo=titulo, Titulo2=titulo2)
+
+@app.route('/painel/index.html')
+def painel():
+    return render_template('painel/index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
