299ac04f4b476c6aa4cff421efec4433bcdec101
realpatocksx <159732016+realpatocksx@users.noreply.github.com>
2024-04-08 21:51:44 +0000

    commit

--- Projetos/EletivaSedu/server/app.py
+++ Projetos/EletivaSedu/server/app.py
@@ -7,21 +7,31 @@ app = Flask(__name__, template_folder=os.path.abspath('Projetos/EletivaSedu/clie
 def login():
     return render_template('login/index.html')
 
+
 @app.route('/cadastro/', methods=['GET', 'POST'])
 def cadastro():        
-    return render_template('cadastro/index.html')
+    if request.method == 'POST':
+        nome_completo = request.form['nome']  
+        print(nome_completo)
+
+    return render_template('cadastro/index.html')c
+
 
 @app.route('/main/', methods=['GET', 'POST'])
 def main():
     return render_template('main/index.html')
 
+ 
 @app.route('/secao/index.html')
 def secao():
     return render_template('secao/index.html')
 
+
 @app.route('/painel/')
 def painel():
     return render_template('painel/index.html')
+
+    
 
 if __name__ == '__main__':
     app.run(debug=True)
