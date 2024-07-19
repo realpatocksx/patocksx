32462a6a2f6baa21bed2130de68b95381df0b4f3
realpatocksx <159732016+realpatocksx@users.noreply.github.com>
2024-07-19 19:58:34 +0000

    commit

--- Server-Material/PYTHON/Flask/Flask-xlsx/server/app.py
+++ Server-Material/PYTHON/Flask/Flask-xlsx/server/app.py
@@ -4,7 +4,7 @@ import pandas as pd
 import os
 
 
-app = Flask(__name__, template_folder=os.path.abspath('Projetos/Flask/client'))
+app = Flask(__name__, template_folder=os.path.abspath('Server-Material/PYTHON/Flask/Flask-xlsx/client'))
 
 # Lista para armazenar os dados das inscrições
 inscricoes = []
@@ -24,13 +24,13 @@ def inscrever():
 
     # Salvar os dados em um arquivo Excel
     df = pd.DataFrame(inscricoes)
-    df.to_excel('Projetos/Flask/server/inscricoes.xlsx', index=False)
+    df.to_excel('Server-Material/PYTHON/Flask/Flask-xlsx/server/inscricoes.xlsx', index=False)
 
     return redirect('/')
 
 @app.route('/dados')
 def listar_dados():
-    df = pd.read_excel('Projetos/Flask/server/inscricoes.xlsx')
+    df = pd.read_excel('Server-Material/PYTHON/Flask/Flask-xlsx/server/inscricoes.xlsx')
     inscricoes = df.to_dict('records')
     return render_template('dados.html', inscricoes=inscricoes)
 
@@ -41,7 +41,7 @@ def detalhar_dado(index):
 
 @app.route('/login')
 def login():
-    df = pd.read_excel('Projetos/Flask/server/inscricoes.xlsx')
+    df = pd.read_excel('Server-Material/PYTHON/Flask/Flask-xlsx/server/inscricoes.xlsx')
 
     # Obtém o valor da terceira célula da coluna A
     valor_A3 = df.iloc[2, 0]
