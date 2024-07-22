ce4e2c5dd281aa4681e36ff69e907ecefcdc0ab9
realpatocksx <159732016+realpatocksx@users.noreply.github.com>
2024-07-22 23:14:51 +0000

    commit

--- Server-Material/PYTHON/Exercicios/Ex25.py
+++ Server-Material/PYTHON/Exercicios/Ex25.py
@@ -1,23 +1,34 @@
 import random
 
-def votacao_eleicao(nome_eleito1, nome_eleito2, nome_eleito3):
-    print('')
-    
-maxvotacao = 7
-voto_eleito1 = 0
-voto_eleito2 = 0
-voto_eleito3 = 0
+def votacao_eleicao(nome_eleito1, nome_eleito2, nome_eleito3):    
+    maxvotacao = 7
+    voto_eleito1 = 0
+    voto_eleito2 = 0
+    voto_eleito3 = 0
 
-while voto_eleito1 + voto_eleito2 + voto_eleito3 < maxvotacao:            
-    eleitores = ['voto_eleito1', 'voto_eleito2', 'voto_eleito3']
-    votaocao_aleatoria = random.choice(eleitores)
+    while voto_eleito1 + voto_eleito2 + voto_eleito3 < maxvotacao:            
+        eleitores = ['voto_eleito1', 'voto_eleito2', 'voto_eleito3']
+        votaocao_aleatoria = random.choice(eleitores)
 
-    if votaocao_aleatoria == 'voto_eleito1':
-        voto_eleito1 += 1
-    elif votaocao_aleatoria == 'voto_eleito2':
-        voto_eleito2 += 1
-    else: voto_eleito3 += 1
+        if votaocao_aleatoria == 'voto_eleito1':
+            voto_eleito1 += 1
+        elif votaocao_aleatoria == 'voto_eleito2':
+            voto_eleito2 += 1
+        else: voto_eleito3 += 1
 
-print(f"voto_eleito1: {voto_eleito1}")
-print(f"voto_eleito2: {voto_eleito2}")
-print(f"voto_eleito3: {voto_eleito3}")
+    print(f"{nome_eleito1}: {voto_eleito1}")
+    print(f"{nome_eleito2}: {voto_eleito2}")
+    print(f"{nome_eleito3}: {voto_eleito3}")
+
+
+    if voto_eleito1 > voto_eleito2 and voto_eleito1 > voto_eleito3:
+        print(f'O {nome_eleito1} é o mais novo eleito')
+    elif voto_eleito2 > voto_eleito1 and voto_eleito2 > voto_eleito3:
+        print(f'O {nome_eleito2} é o mais novo eleito')
+    elif voto_eleito3 > voto_eleito1 and voto_eleito3 > voto_eleito2:
+        print(f'O {nome_eleito3} é o mais novo eleito')
+
+nome1 = input('Informe o nome do eleitor: ')
+nome2 = input('Informe o nome do eleitor: ')
+nome3 = input('Informe o nome do eleitor: ')
+votacao_eleicao(nome1, nome2, nome3)
