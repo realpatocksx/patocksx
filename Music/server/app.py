2314ab5bf3cc5c0f94b1b129ca4a75f10600c3f4
realpatocksx <159732016+realpatocksx@users.noreply.github.com>
2024-08-02 23:30:40 +0000

    commit

--- Music/server/app.py
+++ Music/server/app.py
@@ -3,18 +3,19 @@ import os
 
 app = Flask(__name__, template_folder=os.path.abspath('Music/client'), static_folder=os.path.abspath('Music/client')) 
 
+#Carregar tela inicial
 @app.route('/')
 def index():
     return render_template('index.html')
 
+#Carregar tela playlist
 @app.route('/playlist')
 def playlist():
     return render_template('playlist.html')
 
+#Gerenciar Sistema de musica de add-music
 @app.route('/add_music', methods=['POST'])
 def add_music():
-    music_name = request.form.get('music_name')
-    artist_name = request.form.get('artist_name')
     music_url = request.form.get('music_url')
     # Aqui você pode adicionar lógica para salvar a música adicionada em um banco de dados ou arquivo
     return redirect(url_for('playlist'))
