bdb8e582feedc18444380a731ec8acd077103dec
realpatocksx <159732016+realpatocksx@users.noreply.github.com>
2024-08-28 20:47:26 +0000

    commit

---
+++ Server-Material/PYTHON/execiciosv2/ex01.py
@@ -0,0 +1,16 @@
+                                    #Introducao 
+
+
+#Tipos Dados 
+'''
+Um tipo de dado em uma linguagem de programação define a natureza dos valores 
+que podem ser armazenados e manipulados. Ele especifica a quantidade de memória 
+necessária e o tipo de operações que podem ser realizadas com esses valores. 
+Exemplos comuns de tipos de dados incluem:
+Inteiros: Representam números como "1", "42", ou "-7".
+Ponto flutuante: Representam números reais com casas decimais, como "3.14" ou "-0.001".
+Strings: Representam sequências de caracteres, como "Olá, mundo!".
+Booleanos: Representam valores de verdadeiro ou falso "(true ou false)".
+Listas/Arrays: Coleções ordenadas de valores, que podem ser de tipos variados.
+Objetos: Estruturas que podem conter múltiplos tipos de dados e propriedades, comuns em linguagens orientadas a objetos.'''
+
