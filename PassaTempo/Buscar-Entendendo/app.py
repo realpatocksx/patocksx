01b2b5f68caf074d4f2ba2df7038a8fceeb15c70
realpatocksx <159732016+realpatocksx@users.noreply.github.com>
2024-09-26 13:32:10 +0000

    commit

---
+++ PassaTempo/Buscar-Entendendo/app.py
@@ -0,0 +1,21 @@
+'''
+dados pessoais 
+Nome - Sobrenome - idade renda
+'''
+
+dados = {
+    'CPF': {
+        'nome': 'Nome da Pessoa',
+        'idade': 0,
+        'status_civil': 'Solteiro',
+        'renda': 0
+    },
+
+    'CPF': {
+        'nome': 'Nome',
+        'idade': 0,
+        'status_civil': 'Casado',
+        'renda': 0
+    } 
+ 
+}
