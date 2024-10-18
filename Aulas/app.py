1b778646d13c1f067c25b66e3d12711475cc8121
realpatocksx <159732016+realpatocksx@users.noreply.github.com>
2024-10-18 21:07:00 +0000

    commit

--- Aulas/app.py
+++ Aulas/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template_string, redirect, url_for, flash
+from flask import Flask, request, render_template, redirect, url_for
 
 app = Flask(__name__)
 app.secret_key = 'xss_test'
@@ -12,53 +12,20 @@ def login():
         password = request.form.get('password')
 
         if username in usuarios and usuarios[username] == password:
-            return redirect(url_for('welcome', user=username))
+            return redirect(url_for('welcome', user=username))  # Redireciona para a página de boas-vindas
         else:
-            # Redireciona para uma página de erro, exibindo o nome do usuário fornecido
             return redirect(url_for('login_error', user=username))
 
-    return render_template_string('''
-        <!DOCTYPE html>
-        <html lang="pt-br">
-        <head>
-            <meta charset="UTF-8">
-            <title>Login</title>
-        </head>
-        <body>
-            <h2>Login</h2>
-            <form method="POST">
-                <label>Usuário:</label><br>
-                <input type="text" name="username"><br><br>
-                <label>Senha:</label><br>
-                <input type="password" name="password"><br><br>
-                <button type="submit">Entrar</button>
-            </form>
-        </body>
-        </html>
-    ''')
+    return render_template('login.html')
+
+@app.route('/welcome/<user>')
+def welcome(user):
+    return render_template('welcome.html', user=user)
 
 @app.route('/login_error')
 def login_error():
-    # Exibe o nome do usuário sem escapar, simulando uma vulnerabilidade XSS
     user = request.args.get('user', '')
-    return render_template_string(f'''
-        <!DOCTYPE html>
-        <html lang="pt-br">
-        <head>
-            <meta charset="UTF-8">
-            <title>Erro de Login</title>
-        </head>
-        <body>
-            <h2>Erro de Login</h2>
-            <p>O usuário <b>{user}</b> não pôde ser autenticado.</p>
-            <a href="/">Voltar para o Login</a>
-        </body>
-        </html>
-    ''')
+    return render_template('login_error.html', user=user)
 
 if __name__ == '__main__':
     app.run(debug=True)
-    #'''
-    '''
-    <script>function teste(){alert('funfa')};teste()</script>
-    '''
