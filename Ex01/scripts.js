67e95411f879a2406fa70bcf45726ab268c6e4ed
realpatocksx <159732016+realpatocksx@users.noreply.github.com>
2024-10-20 17:32:22 +0000

    commit

---
+++ Ex01/scripts.js
@@ -0,0 +1,11 @@
+
+
+const botao = document.querySelector('.botao')
+botao.addEventListener('click', function(event){
+    event.preventDefault()
+    let nome_noticia = document.querySelector('#nomedanoticia').value
+    let categoria_noticia = document.querySelector('#categorianoticia').value
+    let descricao_noticia = document.querySelector('#descricaonoticia').value
+    
+    alert('Nome da Noticia: '  + nome_noticia + 'Tipo da Categoria: ' + categoria_noticia + 'Descricao:' + descricao_noticia)
+});
