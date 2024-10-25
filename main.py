9f736b662856208ebece68c1a3e26b6d2acabcd4
realpatocksx <159732016+realpatocksx@users.noreply.github.com>
2024-10-25 20:03:54 +0000

    commit

---
+++ main.py
@@ -0,0 +1,24 @@
+import random 
+
+
+
+user = 'ph'
+password = 'fss'
+
+global users
+global identificador 
+users = {
+    user: {
+        'User': user,
+        'Password': password,
+        'Identificador': identificador
+    },
+
+    'User': {}
+}
+
+def identificador():
+    
+    identificador = 'saa'
+    
+print(users)    
