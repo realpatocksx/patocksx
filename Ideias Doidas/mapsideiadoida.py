7753af1ce3913c4b9af2568177d1d5b8d88414a5
realpatocksx <159732016+realpatocksx@users.noreply.github.com>
2024-11-10 19:12:21 +0000

    commit

---
+++ Ideias Doidas/mapsideiadoida.py
@@ -0,0 +1,9 @@
+casa = 0 #ponto inicial demorar 0minutos opois estou aqui
+lanchonete = 15 #demorar 15minutos para chegar
+farmacia = 11 #demorar 11minutos para chegar
+
+#ponto inicial casa
+if lanchonete > farmacia:
+    print('Entao a farmacia é mais perto')
+elif lanchonete < farmacia:
+    print('Entao a lanchonete é mais perto')
