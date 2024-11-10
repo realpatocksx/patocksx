4709e8470d2695e692201760ed2df8506bee0d47
realpatocksx <159732016+realpatocksx@users.noreply.github.com>
2024-11-10 18:58:24 +0000

    commit

---
+++ testeobjetos/app.js
@@ -0,0 +1,24 @@
+users = {
+    'phznn': {
+        'Username': 'PEDRO CASTRO DA SILVA',
+        'Password': 'phznn123',
+        'Idade': '16'
+    },
+    
+    'duda07': {
+        'Username': 'MARIA SOUZA',
+        'Password': 'duda07',
+        'Idade': '18'
+    },
+
+    'Admin': {
+        'Username': 'admin',
+        'Password': 'admin',
+        'Idade': '999'
+    } 
+}
+const box = document.querySelector('.box')
+box.innerHTML = '';
+users.forEach(event => {
+    console.log(users)
+});
