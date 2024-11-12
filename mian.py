2769a9f4e4aed0bc794a25e50d2f3b72533f67dc
realpatocksx <159732016+realpatocksx@users.noreply.github.com>
2024-11-12 20:05:06 +0000

    commit

--- mian.py
+++ mian.py
@@ -1,4 +1,4 @@
-listadesordenada = [2,0,4,5,7,9,3,8,1,6]
+listadesordenada = [2,1,4,5,7,9,3,8,0,6]
 
 def ordenalista(lista):
     novalista = []
@@ -9,11 +9,16 @@ def ordenalista(lista):
             indice2 = itematual
             novalista = listadesordenada[indice1], listadesordenada[indice2] = listadesordenada[indice2], listadesordenada[indice1]
             print('sim e maior',item,itematual)            
+            itematual += 1
+            print('Item atual era', itematual)
         elif item < itematual:
             indice1 = item 
             indice2 = itematual
             novalista = listadesordenada[indice2], listadesordenada[indice1] = listadesordenada[indice1], listadesordenada[indice2]
             print('nao e maior',item,itematual)
+            itematual -= 1
+            print('Item atual era', itematual)
+            
 
     print(novalista)
-ordenalista(listadesordenada)        
+ordenalista(listadesordenada) 
